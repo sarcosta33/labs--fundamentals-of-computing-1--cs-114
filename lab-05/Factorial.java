a51d5db97429782afa44b17f905ab4cf2815ad21
Roy A Vanegas <roy@thecodeeducators.com>
2023-01-03 12:54:12 -0500

    Add lab 5

---
+++ lab-05/Factorial.java
@@ -0,0 +1,6 @@
+import java.util.Scanner;
+
+public class Factorial {
+  public static void main(String[] args) {
+  }
+}
---
+++ lab-05/LoveCS.java
@@ -0,0 +1,13 @@
+public class LoveCS {
+  public static void main(String[] args) {
+    final int LIMIT = 10;
+
+    int count = 1;
+
+    while (count <= LIMIT) {
+      System.out.println("I love Computer Science!!");
+
+      count++;
+    }
+  }
+}
