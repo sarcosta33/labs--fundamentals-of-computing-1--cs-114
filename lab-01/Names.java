c1ed23942faab934390c2ed7e8cb7f0899df943e
sarcosta33 <sarcosta@hartford.edu>
2023-09-15 15:39:25 -0400

    Added info

--- lab-01/Names.java
+++ lab-01/Names.java
@@ -6,10 +6,13 @@
 public class Names {
   public static void main(String[] args) {
     System.out.println();
-    System.out.println(" \tName\t\tHometown") ;
-    System.out.println("\t====\t\t========");
-    System.out.println("\tSally\t\tRoanoke");
-    System.out.println("\tAlexander\tWashington");
+    System.out.println(" \tName\t\tHometown\t\tMajor") ;
+    System.out.println("\t====\t\t========\t\t=====");
+    System.out.println("\tSally\t\tRoanoke\t\t\tComputer Science");
+    System.out.println("\tAlexander\tWashington\t\tMath");
+    System.out.println("\tSara\t\tShrub Oak\t\tComputer Engineering");
+    System.out.println("\tKelly\t\tNassau\t\t\tElementary Education");
+    System.out.println("\tKarenna\t\tArdsley\t\t\tEducation");
     System.out.println();
   }
 }
