dfc4ec55fbe0020fe2d8201a40ffb1e833cb9de8
sarcosta33 <sarcosta@hartford.edu>
2023-09-15 15:39:09 -0400

    Fixed errors

--- lab-01/Errors2.java
+++ lab-01/Errors2.java
@@ -1,20 +1,19 @@
-#import java.util.Scanner;
+import java.util.Scanner;
 // File:     Errors.java
 // Purpose: A program with lots of syntax errors
 // Correct all of the errors (STUDY the program carefully!!)
-public class errors
-{
-public static void main (String[] args)
-{
-String Name; / Name of the user
-int number;
-int numSq;
-Scanner scan = new Scanner(System.in);
-System.out.print ("Enter your name, please: ")
-Name = scan.nextInt();
-System.out.print ("What is your favorite number?);
-number = scan.nextInt();
-numSq = number * number;
-System.out.println (Name  ", the square of your number is "
-numSquared);
+public class Errors2 {
+    public static void main (String[] args) {
+        String Name; // Name of the user
+        int number;
+        int numSq;
+        Scanner scan = new Scanner(System.in);
+        System.out.println  ("Enter your name, please: ");
+        Name = scan.nextLine();
+        System.out.println ("What is your favorite number?");
+        number = scan.nextInt();
+        numSq = number * number;
+        System.out.println (Name + " the square of your number is " +
+        numSq);
+    }
 }
